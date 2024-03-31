861b4a311089d8716f77ff9acb67416652cd7a74
Vladimir <surorion@mail.ru>
2024-03-31 14:41:27 +0300

    PreparingForTesting

---
+++ node/src/test/java/ru/animals/service/NodeApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.animals.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.animals.NodeApplication;
+
+@SpringBootTest(classes = NodeApplication.class)
+public class NodeApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
