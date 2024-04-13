86827436d15a84c530f75d12971f86d6c13333ba
Vladimir <surorion@mail.ru>
2024-04-13 21:45:05 +0300

    LoadClassCollback-comd
    Загрузка начальных шаблонов
    классов обработчиков ообщений
    кнопочного меню

---
+++ dispatcher/src/main/java/ru/animals/service/collbackCommand/impl/CollbackComdInfoAllAnimalFreeDog.java
@@ -0,0 +1,17 @@
+package ru.animals.service.collbackCommand.impl;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.animals.service.collbackCommand.BaseObject;
+import ru.animals.service.collbackCommand.CommonCollbackService;
+import ru.animals.utilsDEVL.DataFromParserCollback;
+
+public class CollbackComdInfoAllAnimalFreeDog extends BaseObject {
+    @Override
+    public SendMessage apply(CommonCollbackService backServ, Long chartId, DataFromParserCollback dataFromParser) {
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(chartId);
+        sendMessage.setText("Method CollbackComdInfoAllAnimalFreeDog.apply не определен");
+
+        return sendMessage;
+    }
+}
---
+++ dispatcher/src/main/java/ru/animals/service/collbackCommand/impl/CollbackComdReport.java
@@ -0,0 +1,18 @@
+package ru.animals.service.collbackCommand.impl;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.animals.service.collbackCommand.BaseObject;
+import ru.animals.service.collbackCommand.CommonCollbackService;
+import ru.animals.utilsDEVL.DataFromParserCollback;
+
+public class CollbackComdReport extends BaseObject {
+
+    @Override
+    public SendMessage apply(CommonCollbackService backServ, Long chartId, DataFromParserCollback dataFromParser) {
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(chartId);
+        sendMessage.setText("Method CollbackComdReport.apply не определен");
+
+        return sendMessage;
+    }
+}
