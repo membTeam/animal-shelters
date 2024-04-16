8ec3277364fc1eb77f5001bf3e7e16365650eb28
Vladimir <surorion@mail.ru>
2024-04-16 18:52:36 +0300

    CommandServiceRepository2
    пропущенный коммит для
    CollbackComdAllanimalfree

--- dispatcher/src/main/java/ru/animals/collbackCommand/impl/CollbackComdAllanimalfree.java
+++ dispatcher/src/main/java/ru/animals/collbackCommand/impl/CollbackComdAllanimalfree.java
@@ -3,7 +3,6 @@ package ru.animals.collbackCommand.impl;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import ru.animals.collbackCommand.BaseObject;
 import ru.animals.collbackCommand.CommandServiceRepository;
-import ru.animals.collbackCommand.CommonCollbackService;
 import ru.animals.service.serviceParser.DataFromParserCollback;
 
 public class CollbackComdAllanimalfree extends BaseObject {
