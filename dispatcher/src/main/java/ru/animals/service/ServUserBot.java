503954b8c1469ba9d726941e9393be8b5aa0ba8b
Vladimir <surorion@mail.ru>
2024-04-22 14:32:42 +0300

    additionalRefact service
    удаление не нужного пакета из
    идентификатора пакета service

---
+++ dispatcher/src/main/java/ru/animals/service/ServUserBot.java
@@ -0,0 +1,8 @@
+package ru.animals.service;
+
+import ru.animals.service.enumStatusUser.EnumStatusUserBot;
+
+public interface ServUserBot {
+    EnumStatusUserBot statudUserBot(long id);
+
+}
--- dispatcher/src/main/java/ru/animals/service/serviceRepostory/ServUserBot.java
+++
@@ -1,8 +0,0 @@
-package ru.animals.service.serviceRepostory;
-
-import ru.animals.service.serviceRepostory.enumStatusUser.EnumStatusUserBot;
-
-public interface ServUserBot {
-    EnumStatusUserBot statudUserBot(long id);
-
-}
